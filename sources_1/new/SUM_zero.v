f2423fc45bf525a4487d68cc8a5c263627c06645
Pchaves98 <69133830+Pchaves98@users.noreply.github.com>
2020-08-03 22:07:07 -0600

    Update SUM_zero.v

--- sources_1/new/SUM_zero.v
+++ sources_1/new/SUM_zero.v
@@ -4,17 +4,17 @@ module SUM_zero(
     input wire [31:0] OperandoA, OperandoB,
 
     output reg [31:0] SUMout,
-    output reg zero
+    output reg Cero
 
     );
    always @(*) begin
     SUMout=OperandoA+OperandoB;
     
     if(SUMout==32'd0)
-		  zero = 1'b1;
+		  Cero = 1'b1;
 		  
 		else
-		  zero = 1'b0;
+		  Cero = 1'b0;
 		  
 		  end
 endmodule
