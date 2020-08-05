aa477904a5863afdd44c9217ecf8d128fb169b11
darwin <darwinjpr@hotmail.com>
2020-08-05 03:05:14 -0600

    expansión de signo

---
+++ sources_1/new/s_ext.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.08.2020 02:35:24
+// Design Name: 
+// Module Name: s_ext
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module s_ext(input signed [11:0] in, output signed [31:0] out);
+
+    assign out = in;
+
+endmodule
