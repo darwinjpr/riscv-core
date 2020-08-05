a120936b3f8cfa3acf1654b1827917cebce84efa
darwin <darwinjpr@hotmail.com>
2020-08-04 23:00:53 -0600

    Inicializa el banco de registros en 1, (para pruebas iniciales)
    En la implementación final el banco de registros deberías inicializarse todo con ceros

--- sources_1/new/Banco_de_Registros.v
+++ sources_1/new/Banco_de_Registros.v
@@ -5,12 +5,20 @@ input [4:0] DIR_A, DIR_B, DIR_WR; // Registros fuente (rs1, rs2) y registro dest
 input [31:0] DI; // Registro que se va a escribir
 output [31:0] DOA,DOB; //Registros de salida 32 bits
 
-reg [31:0] regfile[0:31];
-
+//reg [31:0] regfile[0:31];
+reg [31:0] regfile [31:0];
+//inicializa banco de registros en cero
+integer i;
+initial begin 
+    for (i = 0; i <= 31; i=i+1) begin // Ciclo for para inicializar cada espacio de memoria
+        regfile[i] = 32'd1;
+end end
 
 assign DOA = regfile[DIR_A];
 assign DOB = regfile[DIR_B];
 
 always @(posedge clk)
-    if(REG_WR) regfile [DIR_WR] <= DI;
+    if(REG_WR == 1) begin
+     regfile [DIR_WR] <= DI;
+     end
 endmodule
