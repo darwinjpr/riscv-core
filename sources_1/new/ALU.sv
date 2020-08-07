0e7f7437e51c6d0d27b8f91982bb81dbb73c30cc
darwin <darwinjpr@hotmail.com>
2020-08-06 19:32:48 -0600

    unidadControl controla ALU
    Elimina texto extra en alu, cambia lógica de obtener cero en alu, adapta control para controlar tambien la alu

--- sources_1/new/ALU.sv
+++ sources_1/new/ALU.sv
@@ -37,14 +37,6 @@ logic salida_carry;         //conecta al carry de la alu
 
 //asignaciones de salidas de alu
 assign ALUResult = ALU_Result; // asiganar los valores de ALU_Result a ALUOU
-//assign FlagAcarreo = salida_carry;
-
-//respuesta al shift izq y derecha
-//wire [N-1:0] r_shift_left, r_shift_right;  //variables auxiliares para shift left y right
-//wire r_a_shift_left, r_a_shift_right;
-//declaracion de modulos
-//deco_shift_left SHIFT_LEFT(ALUA, ALUB, ALUFlagIn, r_shift_left, r_a_shift_left);    //shift left
-//deco_shift_right SHIFT_RIGHT(ALUA, ALUB, ALUFlagIn, r_shift_right, r_a_shift_right); //shift right
 
 //respuesta a la suma y resta    
 logic [N-1:0] ALU_SUMA, ALU_RESTA;
@@ -53,174 +45,34 @@ logic CarrySUMA, CarryRESTA;
 CarryLookAheadSumador #(N) SUM( ALUA, ALUB, ALU_SUMA, CarrySUMA, ALUFlagIn);   
 CarryLookAheadSumadorR #(N) SUM2( ALUA, ALUB, ALU_RESTA, CarryRESTA, ALUFlagIn);
 
-//respuesta al incremento y decremento en 1 y variables locales de entrada   
-//logic [N-1:0] ALU_incremento, ALU_decremento, AA, BB;
-//logic Carry_incremento, Carry_decremento;
-//Declaración de módulos
-//CarryLookAheadSumador #(N) INCREMENTO( AA, BB, ALU_incremento, Carry_incremento, 0);   
-//CarryLookAheadSumadorR #(N) DECREMENTO( AA, BB, ALU_decremento, Carry_decremento, 0);
-
 //La funcion case se utiliza para seleccionar cual resultado se conecta a la salida de la ALU
 //es decir, todas las funciones son llamadas, solo que al final del circuito se escoje cual es la salida
 always @(*)
 begin
-//FlagDesbordamiento = 0;
-//FlagNegativo = 0;
-FlagCero = 0;
+if (ALU_Result=='0)FlagCero =1'b1;
+else FlagCero =1'b0;
+
 salida_carry = 0;
-/*
-    if(ALUFlagIn == 0)
-        begin
-            AA = ALUA;
-            BB = 1;
-        end
-    else
-        begin
-            AA = ALUB;
-            BB = 1;
-        end
-        */
+
     case(ALUControl)
     2'b00: // suma
     begin
         ALU_Result = ALU_SUMA;
         salida_carry = CarrySUMA;
-        FlagCero = (ALU_Result == '0);
-        //overflow
-        /*if (ALUA[N-1] == ALUB[N-1])
-          begin
-             if(ALUA[N-1] != ALU_Result[N-1])
-               FlagDesbordamiento = 1; 
-             else
-               FlagDesbordamiento = 0;
-          end
-        else 
-        FlagDesbordamiento = 0;
-        //bandera negativo
-        FlagNegativo = (ALU_Result[N-1] == 1);*/
+
     end
     2'b01: // resta 
     begin
         ALU_Result = ALU_RESTA;
         salida_carry = CarryRESTA;
-        FlagCero = (ALU_Result == '0);
-        //overflow
-        /*if (ALUA[N-1] != ALUB[N-1])
-          begin
-             if(ALUA[N-1] != ALU_Result[N-1])
-               FlagDesbordamiento = 1; 
-             else
-               FlagDesbordamiento = 0;
-          end
-        else 
-        FlagDesbordamiento = 0;
-        //bandera negativo
-        FlagNegativo = (ALU_Result[N-1] == 1);*/
+
     end
-    /*
-    4'b0010: //  incrementa1 --- suma1
-        begin
-            ALU_Result = ALU_incremento;
-            salida_carry = Carry_incremento;
-            FlagCero = (ALU_Result == '0);
-        end
-    4'b0011: //  decrementa1 --- resta1
-        begin
-            ALU_Result = ALU_decremento;
-            salida_carry = Carry_decremento;
-            FlagCero = (ALU_Result == '0);
-        end
-    4'b0100: // Corrimiento Izquierda
-        if (ALUB == N)
-            begin
-                ALU_Result = {(N){ALUFlagIn}};
-                salida_carry = ALUA[0];
-                FlagCero = (ALU_Result == '0);
-            end
-        else 
-            begin
-            if (ALUB > N)
-                begin
-                    ALU_Result = {(N){ALUFlagIn}};
-                    salida_carry = ALUFlagIn;
-                    FlagCero = (ALU_Result == '0);
-                end
-            else
-                begin
-                    ALU_Result = r_shift_left;
-                    salida_carry = r_a_shift_left;
-                    FlagCero = (ALU_Result == '0);
-                end
-            end
-    4'b0101: // Corrimiento Derecha
-        if (ALUB == N)
-            begin
-                ALU_Result = {(N){ALUFlagIn}};
-                salida_carry = ALUA[N-1];
-                FlagCero = (ALU_Result == '0);
-            end
-        else
-            begin
-            if (ALUB > N)
-                begin
-                    ALU_Result = {(N){ALUFlagIn}};
-                    salida_carry = ALUFlagIn;
-                    FlagCero = (ALU_Result == '0);
-                end
-            else
-                begin
-                    ALU_Result = r_shift_right;
-                    salida_carry = r_a_shift_right;
-                    FlagCero = (ALU_Result == '0);
-                end
-            end
-    4'b0110: //  negación a o b
-        if (ALUFlagIn == 0)
-            begin
-                ALU_Result = ~ ALUA;
-                FlagCero = (ALU_Result == '0);
-            end
-        else
-            begin
-                ALU_Result = ~ ALUB;
-                FlagCero = (ALU_Result == '0);
-            end
-    4'b0111: //  Logical and 
-    begin
-        ALU_Result = ALUA & ALUB;
-        FlagCero = (ALU_Result == '0);
-        FlagNegativo = (ALU_Result[N-1] == 1);
-     end
-    4'b1000: //  Logical or
-    begin
-        ALU_Result = ALUA | ALUB;
-        FlagCero = (ALU_Result == '0);
-        FlagNegativo = (ALU_Result[N-1] == 1);
-    end
-    4'b1001: //  Logical xor  
-    begin
-        ALU_Result = ALUA ^ ALUB;
-        FlagCero = (ALU_Result == '0);
-        FlagNegativo = (ALU_Result[N-1] == 1);
-    end
-    */
+
       default: 
           begin
           ALU_Result = ALU_SUMA;
           salida_carry = CarrySUMA;
-          FlagCero = (ALU_Result == '0);
-          //overflow
-          /*if (ALUA[N-1] == ALUB[N-1])
-            begin
-               if(ALUA[N-1] != ALU_Result[N-1])
-                 FlagDesbordamiento = 1; 
-               else
-                 FlagDesbordamiento = 0;
-            end
-          else 
-          FlagDesbordamiento = 0;
-          //bandera negativo
-          FlagNegativo = (ALU_Result[N-1] == 1);*/
+          
       end 
     endcase
 end
--- sources_1/new/UnidadControl.v
+++ sources_1/new/UnidadControl.v
@@ -2,77 +2,88 @@
 
 module UnidadControl(
     input wire [6:0] opcode,
+    input wire funct7_5,
     input clk,cero,
-    output reg S_Mux_A,
+    output reg [1:0]control_ALU,
+    output  S_Mux_A,
     output reg [1:0] S_Mux_B, S_Mux_C,//señales de control de los muxes
     output reg REG_RD, REG_WR, MEM_RD, MEM_WR
     );
     //wire [6:0] opcode=Instruccion[6:0];
     
+    assign S_Mux_A = cero ? 1'b0 : (opcode[6]?(opcode[5]?(opcode[4]?(1'b0):(opcode[3]?(1'b0):(opcode[2]?(1'b0):(opcode[1]?(opcode[0]?(1'b1):(1'b0)):(1'b0))))):(1'b0)):1'b0) ;
+    
     always @(*) begin
     
     case(opcode)
         7'b1100011: begin//branch
-        if (cero == 1'b1)S_Mux_A=1'b1;
-        else S_Mux_A=1'b0;
+//        if (cero == 1'b1)S_Mux_A=1'b1;
+//        else S_Mux_A=1'b0;
         S_Mux_B=2'b11;
         S_Mux_C=2'b11;
         REG_RD=1'b0;
         REG_WR= 1'b0;
         MEM_RD=1'b0;
         MEM_WR=1'b0; 
+        control_ALU=2'b01;
         end
         
         7'b0110111: begin//lui
-        S_Mux_A=1'b0;
+//        S_Mux_A=1'b0;
         S_Mux_B=2'b11;
         S_Mux_C=2'b00;
         REG_RD=1'b0;
         REG_WR= 1'b1;
         MEM_RD=1'b0;
-        MEM_WR=1'b0; 
+        MEM_WR=1'b0;
+        control_ALU=2'bxx;
         end
         
         7'b0110011: begin//add
-        S_Mux_A=1'b0;
+//        S_Mux_A=1'b0;
         S_Mux_B=2'b00;
         S_Mux_C=2'b01;
         REG_RD=1'b1;
         REG_WR= 1'b1;
         MEM_RD=1'b0;
-        MEM_WR=1'b0; 
+        MEM_WR=1'b0;
+        if (funct7_5 == 1'b1)control_ALU=2'b01;
+        else control_ALU=2'b00;
         end
         
         7'b0010011:begin//addi
-        S_Mux_A=1'b0;
+//        S_Mux_A=1'b0;
         S_Mux_B=2'b01;
         S_Mux_C=2'b01;
         REG_RD=1'b1;
         REG_WR= 1'b1;
         MEM_RD=1'b0;
         MEM_WR=1'b0; 
+        control_ALU=2'b00;
         end
         
         
         7'b0100011:begin//store
-        S_Mux_A=1'b0;
+//        S_Mux_A=1'b0;
         S_Mux_B=2'b10;
         S_Mux_C=2'b11;
         REG_RD=1'b1;
         REG_WR= 1'b0;
         MEM_RD=1'b0;
         MEM_WR=1'b1;
+        control_ALU=2'b00;
         end
         
         
         7'b0000011:begin//load
-        S_Mux_A=1'b0;
+//        S_Mux_A=1'b0;
         S_Mux_B=2'b01;
         S_Mux_C=2'b10;
         REG_RD=1'b1;
         REG_WR= 1'b1;
         MEM_RD=1'b1;
         MEM_WR=1'b0; 
+        control_ALU=2'b00;
         end
      
      
